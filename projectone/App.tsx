09302bf33597e530693e792a06e46450b5bcdae1
Om Saraswat <sharmaom27022004@gmail.com>
2025-02-11 00:30:52 +0530

    .

--- projectone/App.tsx
+++ projectone/App.tsx
@@ -1,14 +1,56 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import { Text, View, Button, Alert } from 'react-native';
+import React, { Component ,useState} from 'react';
+import Companydata from './component/Companydata';
 
-export class App extends Component {
-  render() {
-    return (
-      <View>
-        <Text>dada</Text>
-      </View>
-    )
+const App = () => {
+  
+  
+  const fruit = (val :number) => {
+    console.log("Value:", val); // Try this
+    
+  };
+
+  let data = "Box";
+  const[name,setName] = useState("Boss");
+  function testname() {
+      setName("om");
+      data = "God";
+      console.log("1");
   }
+
+  
+    return (
+      <View style={{ backgroundColor: '#212121', height: '100%', padding: 20 }}>
+        <Text style={{ fontSize: 30, color: '#ffffff', marginBottom: 10 }}>
+          {name}
+        </Text>
+
+        <Userdata />
+
+        <Button title="Click Me" onPress={() => Alert.alert('Button Pressed')} />
+        
+        <Userdata />
+        
+        <Button title="Click Me" onPress={() => testname()} />
+
+        <Companydata /> 
+      </View>
+    );
+  
 }
 
-export default App
+// Functional Component
+const Userdata = () => {
+  return (
+    <View style={{ marginVertical: 10 }}>
+      <Text style={{ fontSize: 20, color: '#ffffff' }}>
+        Name: Om Saraswat
+      </Text>
+      <Text style={{ fontSize: 20, color: '#ffffff' }}>
+        Age: 29
+      </Text>  
+    </View>
+  );
+};
+
+export default App;
---
+++ projectone/component/Companydata.js
@@ -0,0 +1,19 @@
+import {View,Text} from 'react-native'
+
+const Companydata = () =>
+  {
+    return(
+      <View>
+        <Text style = {{fontSize : 20,color:'#ffffff'}}>
+              Name : XYz
+        </Text>
+        <Text style = {{fontSize : 20,color:'#ffffff'}}>
+              
+              Product  : ABC
+        </Text>  
+        
+      </View>
+    )
+  }
+
+  export default Companydata;
